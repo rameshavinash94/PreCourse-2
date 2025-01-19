afbb9fba6d7f1864fd3a0f8599690c2e659f74d9
rameshavinash94 <87649563+rameshavinash94@users.noreply.github.com>
2025-01-18 20:08:53 -0600

    PreCourse2: complet exercise:1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,14 +1,26 @@
+'''
+Time Complexity : 0(log n) -> each iteration divides the search space into half
+Space Complexity : 0(1)
+Did this code successfully run on Leetcode : Yes (704)
+Any problem you faced while coding this : No
+'''
+
 # Python code to implement iterative Binary  
 # Search. 
   
 # It returns location of x in given array arr  
 # if present, else returns -1 
 def binarySearch(arr, l, r, x): 
-  
-  #write your code here
-  
-    
-  
+  while l <= r:
+    mid = (l+r) // 2
+    if arr[mid] == x:
+      return mid
+    elif arr[mid] > x:
+        r = mid - 1
+    else:
+        l = mid + 1
+  return -1
+
 # Test array 
 arr = [ 2, 3, 4, 10, 40 ] 
 x = 10
@@ -17,6 +29,6 @@ x = 10
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print("Element is present at index % d" % result)
 else: 
-    print "Element is not present in array"
+    print("Element is not present in array")
