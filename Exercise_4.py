80df1a62d8b5a9aaf6152ee724cddaf1cc62203a
rameshavinash94 <87649563+rameshavinash94@users.noreply.github.com>
2025-01-18 21:53:37 -0600

    PreCourse2: complete exercise:4

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,13 +1,40 @@
+'''
+Time Complexity : O(nlogn)
+Space Complexity : O(n).
+Did this code successfully run on Leetcode : Yes (#912)
+Any problem you faced while coding this : went thought youtube video/blog on merge sort algo for once prior to coding it out to recall.
+'''
 # Python program for implementation of MergeSort 
 def mergeSort(arr):
-  
-  #write your code here
-  
+  length = len(arr)
+  if length > 1:
+      mid = length//2
+      left = arr[:mid]
+      right = arr[mid:]
+      mergeSort(left)
+      mergeSort(right)
+      i = j = k = 0
+      while i < len(left) and j < len(right):
+          if left[i] < right[j]:
+            arr[k] = left[i]
+            i+=1
+          else:
+            arr[k] = right[j]
+            j+=1
+          k+=1
+      while i < len(left):
+        arr[k] = left[i]
+        i+=1
+        k+=1
+      while j < len(right):
+        arr[k] = right[j]
+        j+=1
+        k+=1
+         
 # Code to print the list 
 def printList(arr): 
-    
-    #write your code here
-  
+    print(arr)
+
 # driver code to test the above code 
 if __name__ == '__main__': 
     arr = [12, 11, 13, 5, 6, 7]  
